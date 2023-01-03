ead7bd2198fc4bd8ce89dd5be77ca6f89d199659
João Bruno <jbrunomf@outlook.com>
2023-01-02 21:14:29 -0400

    Initial commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,49 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("S = Segundo");
+    Console.WriteLine("M = Minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length -1, 1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+
+
+    switch (type.ToString())
+    {
+        case "s":
+            Console.WriteLine("Segundos");
+            Start(time);
+            break;
+        case "m":
+            Console.WriteLine("Minutos");
+            Start(time * 60);
+            break;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+    Menu();
+}
+
+static void Start(int time)
+{
+    int currentTime = 0;
+    
+    while (currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+    
+    Console.Clear();
+    Thread.Sleep(2500);
+    Console.WriteLine("StopWatch finalizado.");
+    Thread.Sleep(2000);
+}
